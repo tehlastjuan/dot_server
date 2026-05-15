de5d1b7cb6322f6d3fd2607d241e675114d17921
Juan Rodrigo <juanrodr100@student.kau.se>
2026-05-15 13:02:51 +0200

    updated route_fwd.sh

--- route_fwd.sh
+++ route_fwd.sh
@@ -1,11 +1,66 @@
 #!/usr/bin/env bash
 
-sudo iptables -t nat -A POSTROUTING -o wlan0 -j MASQUERADE --random
-sudo iptables -A FORWARD -i eth0 -o wlan0 -j ACCEPT
-sudo iptables -A FORWARD -i wlan0 -o eth0 -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
-sudo iptables -A FORWARD -j DROP
+WL_INTERFACE="wlan0"
+ET_INTERFACE="etho0"
 
-# sudo iptables -t nat -A POSTROUTING -o my_modem_interface -j MASQUERADE --random
-# sudo iptables -A FORWARD -i my_lan_interface -o my_modem_interface -j ACCEPT
-# sudo iptables -A FORWARD -i my_modem_interface -o my_lan_interface -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
-# sudo iptables -A FORWARD -j DROP
+ipv4_forwarding() {
+  [ $# -eq 0 ] && return 1
+
+  local -i is_forwarding=0
+  case "${1-}" in
+    --true) is_forwarding=1 ;;
+    --false) : ;;
+  esac
+
+  local forwarding_conf_file tmp_forwarding_conf_file
+  forwarding_conf_file="/etc/sysctl.d/99-forwarding.conf"
+  tmp_forwarding_conf_file=$(mktemp)
+
+  cat > "$tmp_forwarding_conf_file" << EOT
+net.ipv4.ip_forward=${is_forwarding}
+EOT
+
+  # only update if the file doesn't exist or has changed
+  if [ -f "$forwarding_conf_file" ] && cmp -s "$tmp_forwarding_conf_file" "$forwarding_conf_file"; then
+    rm -f "$tmp_forwarding_conf_file"
+    return 0
+  fi
+
+  mv "$tmp_forwarding_conf_file" "$forwarding_conf_file"
+  chmod 644 "$forwarding_conf_file"
+  return 0
+}
+
+function allow_forwarding() {
+  ipv4_forwarding --true
+  sudo iptables -t nat -A POSTROUTING -o "$WL_INTERFACE" -j MASQUERADE --random
+  sudo iptables -A FORWARD -i "$ET_INTERFACE" -o "$WL_INTERFACE" -j ACCEPT
+  sudo iptables -A FORWARD -i "$WL_INTERFACE" -o "$ET_INTERFACE" -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+  sudo iptables -A FORWARD -j DROP
+}
+
+function deny_forwarding() {
+  ipv4_forwarding --false
+  sudo iptables -t nat -D POSTROUTING -o "$WL_INTERFACE" -j MASQUERADE --random
+  sudo iptables -D FORWARD -i "$ET_INTERFACE" -o "$WL_INTERFACE" -j ACCEPT
+  sudo iptables -D FORWARD -i "$WL_INTERFACE" -o "$ET_INTERFACE" -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+  sudo iptables -D FORWARD -j DROP
+}
+
+function list_rules() {
+  sudo iptables --list --verbose --numeric --line-numbers
+  sudo iptables --table nat --list --verbose --numeric --line-numbers
+  printf 'IS FORWARDING: %s\n' "$(sysctl net.ipv4.ip_forward | sed -E 's/.*= //g')"
+}
+
+case "${1-}" in
+  --allow)
+    allow_forwarding
+    ;;
+  --deny)
+    deny_forwarding
+    ;;
+  --list)
+    list_rules
+    ;;
+esac
